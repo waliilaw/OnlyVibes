14c4449ff3a8eab1c814ad5e0a7f3d9164d7e683
Wali <humaidwali20@gmail.com>
2024-11-01 15:35:27 +0530

    Update Home.jsx

--- front/src/Pages/Home.jsx
+++ front/src/Pages/Home.jsx
@@ -2,11 +2,30 @@ import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
 
 
 export function Home (){
-    const {login , register } = useKindeAuth()
+    const {login , isAuthenticated, user} = useKindeAuth()
 
 return (<>
-<button onClick={login} type ="button">Log In</button>
-<button onClick={register} type ="button">Log In 2</button>
+
+{!isAuthenticated && (
+<button onClick={login} type ="button" className="login">Log In</button>
+)}
+{isAuthenticated && ( <div className="space-y-2 mt-10 mb-5">
+          <p>Your name: {user?.given_name}</p>
+          <p>Your email: {user?.email}</p>
+          <img src={user?.picture} alt="user" />
+
+          {/* <pre>{JSON.stringify(user, null, 2)}</pre> */}
+
+          <button
+            onClick={() => logout()}
+            type="button"
+            className="b"
+          >
+            Log out
+          </button>
+        </div>
+      )}
+
 
 
 </>)
