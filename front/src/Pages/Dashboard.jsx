c0d613b4ff9bcbafb8c7b9b071059264296076f1
Wali <humaidwali20@gmail.com>
2024-11-03 15:23:59 +0530

    Update Dashboard.jsx

--- front/src/Pages/Dashboard.jsx
+++ front/src/Pages/Dashboard.jsx
@@ -1,26 +1,17 @@
 import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
-import {useState , useEffect} from 'react'
-import './Dashboard.css'
+import './Dashboard.css';
+import {Handlers} from './handlers/handlers'
 
 export function Dashboard() {
+
     const { login, isLoading, isAuthenticated, user, logout } = useKindeAuth();
-    const [musicLink , setMusicLink ] = useState('')
-    const [musicList , setMusicList ] = useState([])
-    
-    const handleSubmit = function(e){
-        e.preventDefault()
-        if(musicLink){
-            setMusicList([...musicList , { link : musicLink , votes : 0}])
-            setMusicLink('')
-        }
-    }
-    
+
     return (
         <>
             <h1>This is the dashboard</h1>
 
             {isLoading && <h1>Loading...</h1>}
- 
+
             {!isLoading && !isAuthenticated && (
                 <button onClick={login} type="button" className="login">Log In</button>
             )}
@@ -31,27 +22,8 @@ export function Dashboard() {
                     <p>Your email: {user?.email}</p>
                     <img src={user?.picture || 'path/to/default-image.jpg'} alt="User" />
 
-                    <button
-                        onClick={() => logout()}
-                        type="button"
-                        className="logout"
-                    >
-                        Log Out
-                    </button>
-
-                    {/*Here is the music submission form oni-chaan UwU */}
-
-                    <form onSubmit ={handleSubmit}>
-                    <input
-              type="url"
-              placeholder="Enter music link"
-              value={musicLink}
-              onChange={(e) => setMusicLink(e.target.value)}
-              required
-            />
-            <button type="submit">Submit Music</button>
-          </form>
-                    
+                    <button onClick={() => logout()} type="button" className="logout">Log Out</button>
+                    <Handlers />
                 </div>
             )}
         </>
